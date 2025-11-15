cbae223a43f16d7beac3770518e4bc576dcae9fb
Scafu <alessandro.mazzariol04@gmail.com>
2025-11-15 12:55:11 +0100

    Fix Internet connection and change name app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.trophies_app_tracker"
+        applicationId = "com.example.TrophiesTracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
